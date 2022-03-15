5ef39d7f3c02ee9aa47562162896d29c23821cd6
sam-cowing <sam.ryan.cowing@gmail.com>
2022-03-15 19:29:27 -0400

    fixed base urk

--- src/components/Login/authAPI.js
+++ src/components/Login/authAPI.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const baseURL = 'http://127.0.0.1:8000/api/'
+const baseURL = 'http://backend-hocsnest.herokuapp.com/api/'
 
 const axiosConnection = axios.create({
     baseURL: baseURL,
